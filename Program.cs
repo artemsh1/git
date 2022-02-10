f865087686a96c8e754afdcb20c54022ebf3baf0
2 <2@ASUS-I3>
2022-02-10 11:24:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("                  #");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("                 **"); Console.ForegroundColor = ConsoleColor.Blue; Console.WriteLine(" ~");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("                *"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("**");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("               ~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*****");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("                *"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("**");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("               ****"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("              **"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("****"); Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("~~");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("             **********"); Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("~");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("             ************");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("             *****"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" ****");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("            **********"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(" ~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("           *******"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.Write(" *"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("****");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("          ********"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*******");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("         *******"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("**********");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("        ******"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("          **"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("**********"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("**");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("         *"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(" ~~"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("******"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" *********");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("        ~~"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("*"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("**********"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" ******");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("       ****************"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" ******");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("      @"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" ************************");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("     @"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    *****************************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("      *******"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("******************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("     ***********"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("****************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("    *"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("***************************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("   ****"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("*************"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" *************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("  *******"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(" ~~"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("*"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("************************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" **********"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.Write(" ***********************"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("*");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("*"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("***"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("***"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("***"); Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("~~"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("**********************"); Console.ForegroundColor = ConsoleColor.Red; Console.Write("@"); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("**");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("                 |||||");
+            Console.WriteLine("                 |||||");
+            Console.WriteLine("                 |||||");
+            Console.WriteLine("                 |||||");
+            Console.WriteLine("                 |||||");
+        }
+    }
+}
